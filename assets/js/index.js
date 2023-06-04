f7f9b02d16ef7d68ea0537394fa32cd19fba0173
elenalynx <elnlynx@gmail.com>
2023-06-05 01:04:49 +0300

    add part section Goods with owl-carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,24 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        // Настройки карусели
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 50,
+        autoplayHoverPause: true,
+        margin: 20,
+        nav: true,
+        mouseDrag: true,
+        touchDrag: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 4
+            }
+        }
     });
 });
 
@@ -11,6 +28,7 @@ const headerLanguageList = document.querySelector('.header__language-list');
 const headerLanguageArrow = document.querySelector('.arrow');
 
 headerLanguageBtn.addEventListener('click', onOpenLanguageList);
+
 function onOpenLanguageList(e) {
     e.preventDefault()
     headerLanguageArrow.classList.toggle('rotate')
